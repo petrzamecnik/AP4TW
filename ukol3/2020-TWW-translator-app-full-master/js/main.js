918946ab1005d0f74fb05864d4b0dcdd6f8249b3
petrzamecnik <63480916+petrzamecnik@users.noreply.github.com>
2022-04-02 03:24:30 +0200

    done uwu

--- ukol3/2020-TWW-translator-app-full-master/js/main.js
+++ ukol3/2020-TWW-translator-app-full-master/js/main.js
@@ -3,21 +3,31 @@ let button = document.querySelector('#translateBtn');
 let userInput = document.querySelector('#userInput');
 let resultInput = document.querySelector('#result');
 let loadingDiv = document.querySelector('#loading');
-var usrIn = ""
-var res = ""
+// let historySection = document.getElementById("history-section");
+let historyP = document.getElementById("history-p");
+let usrIn = ""
+let res = ""
 
 
-function checkLocalStorage(){
-    if (localStorage.length > 0)
-        return false
-    else
-        return true
+// function addItemToHistory(valueIn, valueOut) {
+//     let numOfItemsInStorage = localStorage.length
+//     localStorage.setItem(valueIn, valueOut)
+// }
+
+function addItemToHistory(valueIn, valueOut) {
+    let numOfItemsInStorage = localStorage.length
+    let text = valueIn + " -> " + valueOut
+    localStorage.setItem(numOfItemsInStorage, text)
 }
 
-function fillHistory() {
-    if (checkLocalStorage()){
-        let p = document.getElementById("history-text")
 
+function loadHistory() {
+    let numOfItemsInStorage = localStorage.length
+    historyP.innerHTML = ""
+
+    for (let i = numOfItemsInStorage - 1; i >= 0; i--) {
+        let text = localStorage.getItem(i)
+        historyP.innerHTML += text + "<br>"
     }
 }
 
@@ -26,7 +36,7 @@ button.onclick = function () {
     // show the loading dialog
     loadingDiv.style.display = 'block';
     // disable translate button
-    button.setAttribute('disabled','disabled');
+    button.setAttribute('disabled', 'disabled');
 
     console.log(userInput.value);
     usrIn = userInput.value;
@@ -40,44 +50,25 @@ button.onclick = function () {
 
     // create the GET request against API to obtain JSON result
     fetch(url)
-    .then(function(response) {
-        // server returns the response, parse it to JSON
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(myJson);
-        // get translation string from JSON, put it in result input
-        resultInput.value = myJson.responseData.translatedText;
-        res = resultInput.value;
+        .then(function (response) {
+            // server returns the response, parse it to JSON
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson);
+            // get translation string from JSON, put it in result input
+            resultInput.value = myJson.responseData.translatedText;
+            res = resultInput.value;
 
+            // my things
+            addItemToHistory(usrIn, res)
+            loadHistory()
 
-        let lsCount = 0
-        let lsText = usrIn + " -> " + res
-        console.log(lsText)
-
-        localStorage.setItem(lsCount, lsText)
-
-        console.log(localStorage)
-
-
-        // hide the loading dialog
-        loadingDiv.style.display = 'none';
-        // enable translate button
-        button.removeAttribute('disabled');
-    });
-
-
-
-    //
-    // if (checkLocalStorage()){
-    //     console.log("LOCAL STORAGE EMPTY")
-    //     localStorage.setItem()
-
-// }
-
-
-
-
+            // hide the loading dialog
+            loadingDiv.style.display = 'none';
+            // enable translate button
+            button.removeAttribute('disabled');
+        });
 }
 
 
